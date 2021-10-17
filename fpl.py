651b2bdb2a950f2838acb9526b4ca5f0399701fb
Alistair Grom <agrom17@gmail.com>
2021-10-17 22:50:26 +0100

    methods added

--- fpl.py
+++ fpl.py
@@ -1,37 +1,32 @@
 import requests
 import pandas as pd
 import numpy as np
-
+import sys
 
 team_id='3833351'
 current_week = str(8)
 
-for i in range(1,9):
-  current_week = str(i)
+players_in_squad_data = []
+players_in_squad_names = []
+players_in_squad__gw_points = []
+id_store_1 = []
+id_store_2 = []
 
+new_name = {}
+new_points = {}
+
+def get_players_in_squad():
   url = "https://fantasy.premierleague.com/api/entry/"+team_id+"/event/"+current_week+"/picks/"
   r = requests.get(url)
   json = r.json()
   json.keys()
 
-  players_in_squad_data = []
   for i in range(14):
     t = json['picks'][i]['element']
     players_in_squad_data.append(t)
-  # t = list(t)[0]
-  # print(t)
+  
 
-  print(players_in_squad_data)
-  print()
-
-  players_in_squad_names = []
-  players_in_squad__gw_points = []
-  id_store_1 = []
-  id_store_2 = []
-
-  new_name = {}
-  new_points = {}
-
+def get_players_in_squad_names():
   url = "https://fantasy.premierleague.com/api/bootstrap-static/"
   r = requests.get(url)
   json = r.json()
@@ -45,7 +40,7 @@ for i in range(1,9):
       id_store_1.append(elements_id)
       new_name[str(elements_id)] = elements_name
 
-
+def get_current_week_points():
   url_live = "https://fantasy.premierleague.com/api/event/"+current_week+"/live/"
   r_live = requests.get(url_live)
   json_live = r_live.json()
@@ -59,6 +54,15 @@ for i in range(1,9):
       new_points[str(id_live)] = points_live
 
 
-  print("="*5+" GAMEWEEK "+current_week+" "+"="*5)
-  for i in range(len(id_store_1)):
-    print(new_name[str(id_store_1[i])]+", "+str(new_points[str(id_store_1[i])]))
+def main():
+    get_players_in_squad()
+    get_players_in_squad_names()
+    get_current_week_points()
+
+    print("="*5+" GAMEWEEK "+current_week+" "+"="*5)
+    for i in range(len(id_store_1)):
+      print(new_name[str(id_store_1[i])]+", "+str(new_points[str(id_store_1[i])]))
+
+if __name__ == "__main__":
+    sys.exit(main())
+
